3560ab291bae5aa9cd4368425d00668f9658db96
Mac <mac@Macs-MacBook-Pro.local>
2023-09-21 14:59:46 +0700

    mint nft

---
+++ mint_nft/tests/test.ts
@@ -0,0 +1,3 @@
+// const testNftTitle = "LuckyTech NFT";
+//   const testNftSymbol = "LTN";
+//   const testNftUri = "https://bafkreieixr42iwe2kqa5adwlrnaqufadb3ys53sy6dbp6niybfetcmqupe.ipfs.nftstorage.link/";
